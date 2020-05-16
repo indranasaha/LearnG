5222b823e655af0e35b4e8885d1c36ae96de9b8a
indranasaha <57320006+indranasaha@users.noreply.github.com>
2020-05-16 17:18:49 +0530

    Update Tutorial1.java

--- Tutorial1.java
+++ Tutorial1.java
@@ -3,6 +3,7 @@ public class Tutorial1{
 	public static void main(String[] args) {
 		System.out.println("First Program");
 		System.out.println("First Line");
+		System.out.println("Second Line");
 
 	}
 
